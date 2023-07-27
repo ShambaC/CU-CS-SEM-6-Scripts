1d3f57d92ff10bdefa1054120b4ca6016927d9e3
Shamba Chowdhury <38806897+ShambaC@users.noreply.github.com>
2023-07-27 09:38:04 +0530

    Updated string_op.pl
    Added substring check

--- string_op.pl
+++ string_op.pl
@@ -9,3 +9,16 @@ palind(List, List).
 palind([_ | Tail], Tail).
 palind([Head | Tail], Acc) :-
     palind(Tail, [Head | Acc]).    
+    
+% Substring check
+% here X is the smol one, S is the large one
+substr(X, S) :-
+    string_to_list(X, XL),
+    string_to_list(S, SL),
+    append(_, T, SL),       % append is a built in function
+    append(XL, _, T),
+    X \= [], !.
+
+% Check the data type
+check_for_num(X) :- number(X).  % check_for_number(10) -> true.
+check_for_str(X) :- string(X).  % check_for_str("hello") -> true.
